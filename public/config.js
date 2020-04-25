ee9ba10269bc2ffb61d27f255546f9e33a7964fc
Niklas <n.thilmont@gmail.com>
2020-04-25 20:08:06 +0200

    updated ID config

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 const TESTBEFUND_API_URL = 'https://api.testbefund.nt-dev.eu:443';
-const TESTBEFUND_PATIENT_URL = 'https://www.mein.testbefund.de';
+const TESTBEFUND_PATIENT_URL = 'https://web.testbefund.nt-dev.eu/#/';
